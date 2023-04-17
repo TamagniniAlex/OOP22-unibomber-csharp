6fd82f453106a25a8b014c61d6f1e411321f07f2
Riccardo Moretti <riccardo.moretti6@studio.unibo.it>
2023-04-17 23:52:09 +0200

    polished movementComponent

--- Moretti/UnibomberGameSolution/UnibomberGame/MovementComponnet/MovementComponent.cs
+++ Moretti/UnibomberGameSolution/UnibomberGame/MovementComponnet/MovementComponent.cs
@@ -11,100 +11,29 @@ using static System.Runtime.InteropServices.JavaScript.JSType;
 #pragma warning disable CS8602
 namespace UnibomberGame
 {
+
     /// <summary>
     /// This component manage bomb placement.
     /// </summary>
     public class MovementComponent : AbstractComponent
     {
-        private Pair<float, float> moveBy = new Pair<float, float>(0f, 0f);
+        readonly float POSITIVE_MOVE = 0.2f;
+        private Pair<float, float> _moveBy = new Pair<float, float>(0f, 0f);
 
         /// <inheritdoc />
         public override void Update()
         {
-                this.Entity.addPosition(moveBy);
-                handleDirection();
-        }
-
-        /**
-         * Given the direction it updates the number of frames spent
-         * in that direction for the animation's sake.
-         */
-        private void handleDirection()
-        {
-            Direction newDirection = Direction.extractDirecion(moveBy).orElse(direction);
-            if (this.direction == newDirection)
-            {
-                this.framesInDirection++;
-            }
-            else
-            {
-                this.framesInDirection = 0;
-                this.direction = newDirection;
-            }
-            if (framesInDirection == Constants.Movement.FRAME_DELAY)
-            {
-                passedFrame++;
-                this.framesInDirection = 0;
-            }
+                this.Entity.addPosition(new Pair<float, float>(_moveBy.GetX, _moveBy.GetY));
         }
 
         /**
          * @param direction the direction to move by
          */
-        public final void moveBy(final Direction direction)
+        public void moveBy(Pair<float, float> direction)
         {
-            this.moveBy = new Pair<>(
-                    direction.getX() * this.getEntity().getSpeed() * globalSpeedMultiplier * Constants.Input.POSITIVE_MOVE,
-                    direction.getY() * this.getEntity().getSpeed() * globalSpeedMultiplier * Constants.Input.POSITIVE_MOVE);
-            if (moveBy.equals(new Pair<Float, Float>(0f, 0f)))
-            {
-                hasMoved = false;
-            }
-            else
-            {
-                hasMoved = true;
-            }
-        }
-
-        /**
-         * @return the direction this entity is facing
-         */
-        public final Direction getDirection()
-        {
-            return this.direction;
-        }
-
-        /**
-         * @return the number of frames spent in one direction
-         */
-        public final int getFrameInDirection()
-        {
-            return this.framesInDirection;
-        }
-
-        /**
-         * @return the number of frames spent in one direction
-         *         keeping in mind the FRAME_DELAY
-         */
-        public final int getPassedFrames()
-        {
-            return this.passedFrame;
-        }
-
-        /**
-         * @param speed the new value of globalSpeedMultiplier
-         */
-        public static void setGlobalSpeedMultiplier(final float speed)
-        {
-            globalSpeedMultiplier = speed;
-        }
-
-        /**
-         * @return whether the entity has moved in the last game frame
-         */
-        public boolean hasMoved()
-        {
-            return this.hasMoved;
+            _moveBy = new Pair<float, float>(
+                    direction.GetX * this.Entity.GetSpeed() * POSITIVE_MOVE,
+                    direction.GetY * this.Entity.GetSpeed() * POSITIVE_MOVE);
         }
 
     }
