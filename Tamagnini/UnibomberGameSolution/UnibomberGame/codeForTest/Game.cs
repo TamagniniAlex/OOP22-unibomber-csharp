3393ceed34c30f9fdd0e0c20ca1a78793444149a
Alex Tamagnini <alex.tamagnini@studio.unibo.it>
2023-04-17 10:56:34 +0200

    Added necessary Game

---
+++ Tamagnini/UnibomberGameSolution/UnibomberGame/codeForTest/Game.cs
@@ -0,0 +1,17 @@
+namespace UnibomberGame
+{
+    public class Game : IGame
+    {
+        public List<IEntity> Entities { get; }
+
+        public Game()
+        {
+            Entities = new List<IEntity>();
+        }
+
+        public void AddEntity<T>(T entity) where T : IEntity => Entities.Add(entity);
+
+        public void RemoveEntity(IEntity entity) => Entities.Remove(entity);
+
+    }
+}
