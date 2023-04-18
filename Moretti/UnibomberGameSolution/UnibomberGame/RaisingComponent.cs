41f9942a148014e9604320ed2ceedd2c1c9f7bec
Riccardo Moretti <riccardo.moretti6@studio.unibo.it>
2023-04-18 20:08:37 +0200

    added raisingComponent

---
+++ Moretti/UnibomberGameSolution/UnibomberGame/RaisingComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnibomberGame
+{
+    internal class RaisingComponent : AbstractComponent
+    {
+     public void update()
+        {
+            this.Entity.Game.AddEntity(new Entity(Type.WALL,this.Entity.Position));
+            this.Entity.Game..remove(entity);
+        }
+
+    }
+
+}
