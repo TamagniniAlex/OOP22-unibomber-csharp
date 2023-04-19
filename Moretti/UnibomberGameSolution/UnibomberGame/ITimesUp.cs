aa3b72d01db0a8a21f94b16dc064122af4f72d4d
Riccardo Moretti <riccardo.moretti6@studio.unibo.it>
2023-04-19 10:06:57 +0200

    Times up updated

--- Moretti/UnibomberGameSolution/UnibomberGame/ITimesUp.cs
+++ Moretti/UnibomberGameSolution/UnibomberGame/ITimesUp.cs
@@ -9,15 +9,15 @@ namespace UnibomberGame
     internal interface ITimesUp
     {
 
-        Game Game {  set; }
+        Game Game { get; set; }
         /// <summary>
         /// makes it so the walls start closing in
         /// </summary>
-        void start(int sizeX, int sizeY);
+        void Start(int sizeX, int sizeY);
 
         /// <summary>
         /// should be called every frame. Places a new wall inside the game
         /// </summary>
-        void update();
+        void Update();
     }
 }
--- Moretti/UnibomberGameSolution/UnibomberGame/TimesUp.cs
+++ Moretti/UnibomberGameSolution/UnibomberGame/TimesUp.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,25 +10,60 @@ namespace UnibomberGame
 {
     internal class TimesUp : ITimesUp
     {
-        private bool isStarted;
-        private bool isDone;
-        private int normalizedFrames;
-        private Pair<int, int>? currentPosition;
-        private bool[][]? raisedWalls;
-        private Pair<int, int>? currentDirection;
+        private bool _isStarted;
+        private bool _isDone;
+        private readonly int _startX, _startY;
+        private Pair<int, int> _currentPosition;
+        private bool[,] _raisedWalls;
+        private Pair<int, int> _currentDirection;
 
         /// <inheritdoc />
-        public Game Game {  set; }
-        public void start(int sizeX, int sizeY)
+        public Game Game { get; set; }
+
+        public TimesUp(Game game)
         {
-            currentPosition = new Pair<int, int>(0, 0);
-            currentDirection = new Pair<int, int>(1, 0);
-            raisedWalls = new int[sizeX][sizeY];
+            _isStarted = false;
+            _isDone = false;
+            _currentDirection = new Pair<int, int>(1,0);
+            _currentPosition = new Pair<int, int>(-1, 0);
+            Game = game;
+            Start(10,10);
+        }
+        public void Start(int sizeX, int sizeY)
+        {
+            _raisedWalls = new bool[sizeX,sizeY];
+            _isStarted = true;
         }
 
-        public void update()
+        public void Update()
         {
-            throw new NotImplementedException();
+            if (_isStarted && !_isDone)
+            {
+                Pair<int,int> newPosition = new Pair<int, int>(_currentDirection.GetX + _currentPosition.GetX,
+                          _currentDirection.GetY + _currentPosition.GetY);
+                if (!Utilities.IsBetween(newPosition.GetX, 0, _startX)
+                          || !Utilities.IsBetween(newPosition.GetY, 0, _startY)
+                          || _raisedWalls[newPosition.GetX,newPosition.GetY])
+                {
+                    _currentDirection = getNextClockwise(_currentDirection);
+                    newPosition = new Pair<int,int>(_currentDirection.GetX + _currentPosition.GetX,
+                              _currentDirection.GetY + _currentPosition.GetY);
+                    if (_raisedWalls[newPosition.GetX,newPosition.GetY])
+                    {
+                        _isDone = true;
+                    }
+                }
+                _raisedWalls[newPosition.GetX,newPosition.GetY] = true;                
+                Game.AddEntity(new Entity(Type.RAISING_WALL,new Pair<double, double>(_currentPosition.GetX, _currentPosition.GetY);
+                _currentPosition = newPosition;
+            }
+        }
+        private Pair<int,int> getNextClockwise(Pair<int, int> direction)
+        {
+            int x = direction.GetY > 0 ? 1 : direction.GetY < 0 ? -1 : 0;
+            int y = direction.GetX > 0 ? -1 : direction.GetX < 0 ? 1 : 0;
+
+            return new Pair<int, int>(x, y);
         }
     }
 }
